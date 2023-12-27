cf416fa70f3a1155dbc9b41f4a0585e33d5181ab
Uktam1998 <rakhmatullayev2019@mail.ru>
2023-12-27 15:49:08 +0500

    added news part

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html/"],
+  content: ["*.html/"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")]
 }
 
